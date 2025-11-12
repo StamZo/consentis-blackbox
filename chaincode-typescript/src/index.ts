58a285102151c411eb65c7193ae7c8b72646246a
StamZo <112801706+StamZo@users.noreply.github.com>
2025-11-12 10:46:23 +0200

    Add files via upload

---
+++ chaincode-typescript/src/index.ts
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { VcAnchorContract } from './assetTransfer';
+
+
+export const contracts: any[] = [ VcAnchorContract ];
+
+//export const contracts: any[] = [AgreementContract,CollaborationContract];
+
+//export const contracts: any = {AgreementContract, CollaborationContract};
+
+
